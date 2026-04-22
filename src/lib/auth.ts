cb9ece1f8f73cd61f49a740cd7fd6f97b383c671
Jose Vasquez <joseevb@protonmail.com>
2026-04-22 23:13:20 +0200

    chore: update dependencies (better-auth, nitro, typescript, etc.)

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -8,14 +8,33 @@ import * as schema from '@/db/schemas'
 import { env } from '@/env/server'
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, { provider: 'sqlite', schema }),
+  database: drizzleAdapter(db, { provider: 'sqlite', schema, usePlural: true }),
   baseURL: env.BETTER_AUTH_URL,
   emailAndPassword: { enabled: true },
   socialProviders: {
     google: {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
+      mapProfileToUser: (profile) => ({
+        firstName: profile.name.split(' ')[0],
+        lastName: profile.name.split(' ')[1],
+      }),
     },
   },
   plugins: [admin(), tanstackStartCookies()],
+  user: {
+    deleteUser: {}, // TODO: Add logic for deleting users. See [TODO for more info](TODO.local.md#delete-user)
+    additionalFields: {
+      firstName: {
+        type: 'string',
+        required: true,
+        index: true,
+      },
+      lastName: {
+        type: 'string',
+        required: true,
+        index: true,
+      },
+    },
+  },
 })
