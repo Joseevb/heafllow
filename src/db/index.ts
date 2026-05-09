135552f6fc14e2e9ce933605bb2796e739aee302
Jose Vasquez <joseevb@protonmail.com>
2026-05-09 22:56:38 +0200

    refactor: simplify database connection initialization

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,5 @@
-import { Database } from 'bun:sqlite'
 import { drizzle } from 'drizzle-orm/bun-sqlite'
 
 import { env } from '@/env/server'
 
-const sqlite = new Database(env.DB_FILE_NAME!)
-export const db = drizzle({ client: sqlite })
+export const db = drizzle(env.DB_FILE_NAME)
