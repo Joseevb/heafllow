951b27539351041e4e1dc928e239e2365dfdda9c
Jose Vasquez <joseevb@protonmail.com>
2026-05-24 21:57:48 +0200

    feat: added missing file

---
+++ src/db/migrate.ts
@@ -0,0 +1,7 @@
+import { migrate } from 'drizzle-orm/bun-sqlite/migrator'
+
+import { db } from '.'
+
+migrate(db, { migrationsFolder: './drizzle' })
+
+console.log('Migrations applied successfully')
